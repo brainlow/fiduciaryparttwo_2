182801b06ef955afc513893bd8c7b3dfebb64e21
Raghu Betina <raghu@firstdraft.com>
2018-07-02 16:46:41 +0000

    Generate transactions resource with starter:resource

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,93 @@
+class TransactionsController < ApplicationController
+  def index
+    @transactions = Transaction.all
+
+    render("transactions/index.html.erb")
+  end
+
+  def show
+    @transaction = Transaction.find(params[:id])
+
+    render("transactions/show.html.erb")
+  end
+
+  def new
+    @transaction = Transaction.new
+
+    render("transactions/new.html.erb")
+  end
+
+  def create
+    @transaction = Transaction.new
+
+    @transaction.asset_id = params[:asset_id]
+    @transaction.shares = params[:shares]
+    @transaction.date = params[:date]
+    @transaction.amount = params[:amount]
+    @transaction.category = params[:category]
+    @transaction.description = params[:description]
+    @transaction.payee = params[:payee]
+    @transaction.distribution = params[:distribution]
+
+    save_status = @transaction.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/transactions/new", "/create_transaction"
+        redirect_to("/transactions")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Transaction created successfully.")
+      end
+    else
+      render("transactions/new.html.erb")
+    end
+  end
+
+  def edit
+    @transaction = Transaction.find(params[:id])
+
+    render("transactions/edit.html.erb")
+  end
+
+  def update
+    @transaction = Transaction.find(params[:id])
+
+    @transaction.asset_id = params[:asset_id]
+    @transaction.shares = params[:shares]
+    @transaction.date = params[:date]
+    @transaction.amount = params[:amount]
+    @transaction.category = params[:category]
+    @transaction.description = params[:description]
+    @transaction.payee = params[:payee]
+    @transaction.distribution = params[:distribution]
+
+    save_status = @transaction.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/transactions/#{@transaction.id}/edit", "/update_transaction"
+        redirect_to("/transactions/#{@transaction.id}", :notice => "Transaction updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Transaction updated successfully.")
+      end
+    else
+      render("transactions/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @transaction = Transaction.find(params[:id])
+
+    @transaction.destroy
+
+    if URI(request.referer).path == "/transactions/#{@transaction.id}"
+      redirect_to("/", :notice => "Transaction deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Transaction deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Transaction resource:
+  # CREATE
+  get "/transactions/new", :controller => "transactions", :action => "new"
+  post "/create_transaction", :controller => "transactions", :action => "create"
+
+  # READ
+  get "/transactions", :controller => "transactions", :action => "index"
+  get "/transactions/:id", :controller => "transactions", :action => "show"
+
+  # UPDATE
+  get "/transactions/:id/edit", :controller => "transactions", :action => "edit"
+  post "/update_transaction/:id", :controller => "transactions", :action => "update"
+
+  # DELETE
+  get "/delete_transaction/:id", :controller => "transactions", :action => "destroy"
+  #------------------------------
+
   # Routes for the Beneficiary resource:
   # CREATE
   get "/beneficiaries/new", :controller => "beneficiaries", :action => "new"
