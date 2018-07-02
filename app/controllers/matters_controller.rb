257845e96b6c7f71f59d2cc327462a55e53375b3
Raghu Betina <raghu@firstdraft.com>
2018-07-02 16:47:29 +0000

    Only matter user can access show page

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -1,5 +1,5 @@
 class MattersController < ApplicationController
-  before_action :current_user_must_be_matter_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_matter_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_matter_user
     matter = Matter.find(params[:id])
