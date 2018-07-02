bab64151a5372d6528767124d6f7a12d79e8b1c9
Raghu Betina <raghu@firstdraft.com>
2018-07-02 16:47:09 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "matters#index"
   # Routes for the Transaction resource:
   # CREATE
