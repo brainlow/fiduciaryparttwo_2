5e8ec7e34a9472c9f1f4957002ca22dbaf6aba6d
Raghu Betina <raghu@firstdraft.com>
2018-07-02 16:47:22 +0000

    Whitelist columns in Fiduciary for ActiveAdmin

--- app/admin/fiduciaries.rb
+++ app/admin/fiduciaries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Fiduciary do
+
+ permit_params :first_name, :last_name, :address, :matter_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
